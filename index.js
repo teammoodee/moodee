1778e2855475e1cdc92897b3819b67911965e196
Alek <alekmitrevski@hotmail.com>
2014-10-16 21:49:32 -0700

    blah2

--- index.js
+++ index.js
@@ -6,14 +6,48 @@
         $(document).ready(function () {
             app.initialize();
 
-            getSubject();
+            sendRequest();
         });
     };
 
-    function getSubject() {
-        Office.context.mailbox.item.subject.getAsync(function (result) {
-            app.showNotification('The current subject is', result.value)
-        });
+    function getSubjectRequest(id) {
+        // Return a GetItem operation request for the subject of the specified item. 
+        var result =
+     '<?xml version="1.0" encoding="utf-8"?>' +
+     '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
+     '               xmlns:xsd="http://www.w3.org/2001/XMLSchema"' +
+     '               xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"' +
+     '               xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">' +
+     '  <soap:Header>' +
+     '    <RequestServerVersion Version="Exchange2013" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" soap:mustUnderstand="0" />' +
+     '  </soap:Header>' +
+     '  <soap:Body>' +
+     '    <GetItem xmlns="http://schemas.microsoft.com/exchange/services/2006/messages">' +
+     '      <ItemShape>' +
+     '        <t:BaseShape>IdOnly</t:BaseShape>' +
+     '        <t:AdditionalProperties>' +
+     '            <t:FieldURI FieldURI="item:Subject"/>' +
+     '        </t:AdditionalProperties>' +
+     '      </ItemShape>' +
+     '      <ItemIds><t:ItemId Id="' + id + '"/></ItemIds>' +
+     '    </GetItem>' +
+     '  </soap:Body>' +
+     '</soap:Envelope>';
+
+        return result;
+    }
+
+    function sendRequest() {
+        var mailbox = Office.context.mailbox;
+        mailbox.makeEwsRequestAsync(getSubjectRequest(mailbox.item.itemId), callback);
+    }
+
+    function callback(asyncResult) {
+        var result = asyncResult.value;
+        var context = asyncResult.context;
+
+        console.log(result);
+        console.log(context);
     }
 
 })();
