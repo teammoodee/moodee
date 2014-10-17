4e144b7f2b259a6083f6c3f392daa22d5706aed9
Jonathan Roig <jroig@lm127014-jroig.jomax.paholdings.com>
2014-10-17 11:06:33 -0700

    loading...

--- moodee.js
+++ moodee.js
@@ -13,39 +13,39 @@ function buildDisplay(responseObj)
 	{
 		case 'affection_friendliness':
 			$('#dominantEmotion').addClass('affection');
-			//$('#dominantEmotionText').html('Affection / Friendliness');
+			$('#dominantEmotionExtraText').html('Affection / Friendliness');
 			break;
 		case 'anger_loathing':
 			$('#dominantEmotion').addClass('anger');
-			//$('#dominantEmotionText').html('Anger / Loathing');
+			$('#dominantEmotionText').html('Anger / Loathing');
 			break;
 		case 'contentment_gratitude':
 			$('#dominantEmotion').addClass('contentment');
-			//$('#dominantEmotionText').html('Contentment / Gratitude');
+			$('#dominantEmotionExtraText').html('Contentment / Gratitude');
 			break;
 		case 'enjoyment_elation':
 			$('#dominantEmotion').addClass('enjoyment');
-			//$('#dominantEmotionText').html('Enjoyment / Elation');
+			$('#dominantEmotionExtraText').html('Enjoyment / Elation');
 			break;
 		case 'fear_uneasiness':
 			$('#dominantEmotion').addClass('fear');
-			//$('#dominantEmotionText').html('Fear Uneasiness');
+			$('#dominantEmotionExtraText').html('Fear / Uneasiness');
 			break;
 		case 'humiliation_shame':
 			$('#dominantEmotion').addClass('humiliation');
-			//$('#dominantEmotionText').html('Humiliation / Shame');
+			$('#dominantEmotionExtraText').html('Humiliation / Shame');
 			break;
 		case 'sadness_grief':
 			$('#dominantEmotion').addClass('sadness');
-			//$('#dominantEmotionText').html('Sadness / Grief');
+			$('#dominantEmotionExtraText').html('Sadness / Grief');
 			break;
 		case 'amusement_excitement':
 			$('#dominantEmotion').addClass('amusement');
-			//$('#dominantEmotionText').html('Amusement / Excitement');
+			$('#dominantEmotionExtraText').html('Amusement / Excitement');
 			break;
 		case 'Neutral':
 			$('#dominantEmotion').addClass('neutral');
-			//$('#dominantEmotionText').html('Amusement / Excitement');
+			$('#dominantEmotionExtraText').html('Amusement / Excitement');
 			break;
 	}
 
@@ -63,6 +63,7 @@ function buildDisplay(responseObj)
 	}
 
 	// sentiment
+
 	$('#sentiment').html(responseObj.article_sentiment.sentiment);
 
 	// clarity
@@ -85,11 +86,13 @@ function buildDisplay(responseObj)
 	$('#sadness_grief').html(responseObj.sadness_grief);
 }
 
+
 function showAdditionalInfo()
 {
 	$('#emotionColor').hide();
 	$('#extraInformation').show();
 }
+
 
 function hideAdditionalInfo()
 {
@@ -97,12 +100,10 @@ function hideAdditionalInfo()
 	$('#extraInformation').hide();
 }
 
+
+// init
 $( document ).ready(function()
 {
-	var response = {"article":"I think you should get off your pedestal.","ignored_terms":[],"affection_friendliness":0.13,"enjoyment_elation":0.34,"amusement_excitement":0.25,"contentment_gratitude":0.57,"sadness_grief":-6.35,"anger_loathing":-10,"fear_uneasiness":-0.08,"humiliation_shame":-1.76,"dominant_emotion":"anger_loathing","average_intensity":1.21,"article_sentiment":{"sentiment":"Negative","score":-10},"coverage":95,"intense_sentence":{"sentence":"I think you should get off your pedestal.","dominant_emotion":"anger_loathing","intensity":10},"clarity":94.19};
-	//buildDisplay(response);
-
 	$('#emotionColor').click(showAdditionalInfo);
 	$('#extraInformation').click(hideAdditionalInfo);
-
 });
