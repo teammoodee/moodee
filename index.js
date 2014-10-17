206a7d04fc0f2c7097c198d6b1e89698a651c1c7
Alek <alekmitrevski@hotmail.com>
2014-10-16 20:13:08 -0700

    added initialization for o365 app.

---
+++ index.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+        });
+    };
+
+    function setSubject() {
+        Office.context.mailbox.item.subject.setAsync("Hello world!");
+    }
+
+    function getSubject() {
+        Office.context.mailbox.item.subject.getAsync(function (result) {
+            app.showNotification('The current subject is', result.value)
+        });
+    }
+
+    function addToRecipients() {
+        Office.context.mailbox.item.to.addAsync([Office.context.mailbox.userProfile.emailAddress]);
+    }
+
+})();
