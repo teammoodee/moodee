6579412046f4672889a7ca0f282de4d991dd7f01
Jonathan Roig <jroig@lm127014-jroig.jomax.paholdings.com>
2014-10-16 22:43:30 -0700

    new stuff

--- index.js
+++ index.js
@@ -70,7 +70,6 @@
         var lymbix = new $.lymbix('8c1a36ec73827cd2cfac9d943d981cbacf671d65');
         lymbix.tonalize(content, function (object)
             {
-                blah = object;
                 console.log(object);
                 buildDisplay(object);
             });
